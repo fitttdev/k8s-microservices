1d5d6725fc1a3cb77d10ad99858f4439fa651783
fitttdev <nima.yonten1729@gmail.com>
2024-03-11 20:05:49 +0600

    update scripts

--- scripts/kill.sh
+++ scripts/kill.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-kubectl delete deployment backend1-deployment backend2-deployment frontend-deployment gateway-deployment
-kubectl delete service backend1-service backend2-service frontend-service gateway-service
+kubectl delete -f k8s/
